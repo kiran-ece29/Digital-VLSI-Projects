c226d3396a468ab7326288ef3419bae339fb36ad
kona kiran ram chandra <kk24ecb0b29@student.nitw.ac.in>
2026-01-15 21:54:01 +0530

    Update tb.v

--- Traffic-Light-Controller/tb.v
+++ Traffic-Light-Controller/tb.v
@@ -1,12 +1,14 @@
 `timescale 1ns/1ps
-
 module tb_traffic_light_4way;
 
-    reg clk;
-    reg rst;
+    reg clk = 0;
+    reg rst = 1;
     wire [1:0] r1, r2, r3, r4;
 
-    traffic_light_4way dut (
+    traffic_light_4way #(
+        .GREEN_TIME(5),
+        .ALL_RED_TIME(2)
+    ) dut (
         .clk(clk),
         .rst(rst),
         .r1(r1),
@@ -18,26 +20,15 @@ module tb_traffic_light_4way;
     always #5 clk = ~clk;
 
     initial begin
-        mclk = 0;
-        rst  = 1;
-
         #20 rst = 0;
-        #300;
-
-        rst = 1;
-        #15 rst = 0;
-        #300;
-
-        rst = 1;
-        #10 rst = 0;
-        #300;
-
+        #800;
+        $display("\nSimulation finished");
         $finish;
     end
 
     initial begin
-        $monitor("Time=%0t | R1=%b R2=%b R3=%b R4=%b",
-                 $time, r1, r2, r3, r4);
+        $monitor("T=%4t | state=%0d | timer=%2d | R1=%b R2=%b R3=%b R4=%b",
+                 $time, dut.state, dut.timer, r1, r2, r3, r4);
     end
 
 endmodule
